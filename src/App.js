744d17c8b4f87ea975488912bedc2a95284e832c
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2023-07-30 21:25:21 +0530

    Web3uikit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
 import Header from "./components/Header";
+import Connect from "./components/Connect";
+import LotteryEntrance from "./components/LotteryEntrance";
 function App() {
   return (
     <div>
-      <Header />
+      {/* <Header /> */}
+      <Connect />
+      <LotteryEntrance />
     </div>
   );
 }
---
+++ src/components/Connect.jsx
@@ -0,0 +1,14 @@
+import { ConnectButton } from "web3uikit";
+
+import React from "react";
+
+const Connect = () => {
+  return (
+    <div>
+      <h1>Decentralized Lottery</h1>
+      <ConnectButton />
+    </div>
+  );
+};
+
+export default Connect;
