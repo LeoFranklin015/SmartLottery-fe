398b45dbe4cede040bf93c66fbe0d71f92ef5a7e
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2023-08-01 18:17:50 +0530

    Entering lottery and sending notification

--- src/components/LotteryEntrance.jsx
+++ src/components/LotteryEntrance.jsx
@@ -1,17 +1,107 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "web3uikit";
 import { useWeb3Contract } from "react-moralis";
+import abi from "../constants/abi.json";
+import contractAddress from "../constants/contractAddress.json";
+import console from "console-browserify";
+// import { abi, contractAddress } from "../constants/index";
+import { useMoralis } from "react-moralis";
+import { ethers } from "ethers";
+import { useNotification } from "web3uikit";
+
 const LotteryEntrance = () => {
-  //   const { runContractFunction: enterRaffle } = useWeb3Contract({
-  //     abi://,
-  //     contractAddress: //,
-  //     functionName : //,
-  //     params :{},
-  //     msgValue: //,
-  //   });
+  const [entranceFee, setentranceFee] = useState("0");
+  const [noOfPlayers, setnoOfPlayers] = useState("0");
+  const [RecentWinner, setRecentWinner] = useState("0");
+  const { chainId: chainIdHex, isWeb3Enabled } = useMoralis();
+  const chainId = parseInt(chainIdHex);
+  const raffleAddress =
+    chainId in contractAddress ? contractAddress[chainId][0] : null;
+  const { runContractFunction: enterRaffle } = useWeb3Contract({
+    abi: abi,
+    contractAddress: raffleAddress,
+    functionName: "enterRaffle",
+    params: {},
+    msgValue: entranceFee,
+  });
+
+  const { runContractFunction: getEntranceFee } = useWeb3Contract({
+    abi: abi,
+    contractAddress: raffleAddress,
+    functionName: "getEntranceFee",
+    params: {},
+  });
+
+  const { runContractFunction: getRecentWinner } = useWeb3Contract({
+    abi: abi,
+    contractAddress: raffleAddress,
+    functionName: "getRecentWinner",
+    params: {},
+  });
+  const { runContractFunction: getNumberOfPlayers } = useWeb3Contract({
+    abi: abi,
+    contractAddress: raffleAddress,
+    functionName: "getNumberOfPlayers",
+    params: {},
+  });
+  const dispatch = useNotification();
+  const update = async () => {
+    const fee = await getEntranceFee();
+    const numplayer = await getNumberOfPlayers();
+    const recentWinner = await getRecentWinner();
+    setentranceFee(fee.toString());
+    setnoOfPlayers(numplayer.toString());
+    setRecentWinner(recentWinner);
+  };
+  useEffect(() => {
+    if (isWeb3Enabled) {
+      update();
+    }
+  }, [isWeb3Enabled]);
+
+  const enterLottery = async () => {
+    await enterRaffle({
+      onSuccess: handleSuccess,
+      onError: (error) => console.log(error),
+    });
+  };
+
+  const handleSuccess = async (tx) => {
+    try {
+      console.log("entering");
+      await tx.wait(1);
+      update();
+      handleNotification(tx);
+      console.log("sucess");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleNotification = () => {
+    dispatch({
+      type: "info",
+      message: "Transaction Completed",
+      title: "Notification",
+      position: "topR",
+      icon: "bell",
+    });
+  };
   return (
     <div>
-      <Button>Enter Lottery</Button>
+      <button onClick={enterLottery}> Enter Lottery</button>
+
+      {raffleAddress ? (
+        <h2>
+          Entrance Fee : {ethers.utils.formatUnits(entranceFee, "ether")} ETH{" "}
+          <br />
+          No of players : {noOfPlayers}
+          <br />
+          Recent Winner : {RecentWinner}
+        </h2>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 import { MoralisProvider } from "react-moralis";
+import { NotificationProvider } from "web3uikit";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <MoralisProvider initializeOnMount={false}>
-      <App />
+      <NotificationProvider>
+        <App />
+      </NotificationProvider>
     </MoralisProvider>
   </React.StrictMode>
 );
